2678103157c65bb08e3ce65b7339875d6be2765e
jocxfin <19174728+jocxfin@users.noreply.github.com>
2024-03-26 20:01:47 +0200

    Fix service worker. Incorrect file path

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,19 +1,25 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/static/styles.css',
         '/static/favicon.png',
+        '/static/manifest.json',
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true}).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open('pwa-cache').then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
